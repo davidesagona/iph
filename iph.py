b4bc91e556676f4eaf9fe4e94e1a53b34563a3a9
davidesagona <145986253+davidesagona@users.noreply.github.com>
2024-07-13 04:02:23 +0200

    Update iph.py

--- iph.py
+++ iph.py
@@ -1,23 +1,204 @@
 import requests
 from bs4 import BeautifulSoup
 import re
+import mailtrap as mt
+from datetime import datetime, timezone, timedelta
+import os
+from googleapiclient.discovery import build
+from google.oauth2 import service_account
+import json
+import time
 
-link = 'https://www.subito.it/annunci-italia/vendita/telefonia/?q=iphone&qso=true&shp=true&order=datedesc&advt=0&pht=1'
-page = requests.get(link)
+
+def update_document_content(document_id, new_content):
+    # Prima rimuovi tutto il contenuto esistente
+    document = service.documents().get(documentId=document_id).execute()
+    end_index = document['body']['content'][-1]['endIndex']
+    requests = [
+        {
+            'deleteContentRange': {
+                'range': {
+                    'startIndex': 1,
+                    'endIndex': end_index - 1
+                }
+            }
+        },
+        {
+            'insertText': {
+                'location': {
+                    'index': 1,
+                },
+                'text': new_content
+            }
+        }
+    ]
+
+    result = service.documents().batchUpdate(documentId=document_id, body={'requests': requests}).execute()
+    return result
+
+def aggiorna_file():
+    if not new_content == '':
+        contenuto_file_iniziale = contenuto()
+        da_mettere = new_content + contenuto_file_iniziale
+        da_mettere = da_mettere.replace(' ', '')
+        da_mettere = da_mettere.replace('\n', '')
+            
+        if len(da_mettere) > 25000:
+            da_mettere = da_mettere[:25000]
+        
+        contat = 0
+        while contat <= 5:
+            contat += 1
+            try:
+                update_document_content(DOCUMENT_ID, da_mettere)
+                print("Aggiornamento riuscito")
+                break
+            except:
+                time.sleep(1)
+                if n == 6:
+                    print("Aggiornamento NON riuscito")
+            
+
+def contenuto():
+    contat = 0
+    contenuto_file = ''
+    while contat <= 5:
+        contat += 1
+        try:
+            document = service.documents().get(documentId=DOCUMENT_ID).execute()
+            content = document.get('body').get('content')
+            for element in content:
+                if 'paragraph' in element:
+                    elements = element['paragraph']['elements']
+                    for e in elements:
+                        contenuto_file += e['textRun']['content']
+            print("Lettura riuscita")
+            break
+        except:
+            time.sleep(1)
+            if contat == 6:
+                print("Lettura NON riuscita")
+    return contenuto_file
+
+
+def manda_mail(link_annuncio, titolo='Annuncio interessante'):
+
+    now = datetime.now()
+
+    inizio_ora_legale = datetime(now.year, 3, 31)
+    fine_ora_legale = datetime(now.year, 10, 27)
+
+    if inizio_ora_legale <= now <= fine_ora_legale:
+        ora_attuale = datetime.now(timezone(timedelta(hours=2)))
+    else:
+        ora_attuale = datetime.now(timezone(timedelta(hours=1)))
+
+    formato_leggibile = str(ora_attuale.strftime("%H:%M"))
+
+    mail = mt.Mail(
+        sender=mt.Address(email="mailtrap@demomailtrap.com", name="Tracker Subito.it"),
+        to=[mt.Address(email=MAIL_MIA)],
+        subject=titolo,
+        text='Orario: ' + formato_leggibile + '\n\nLink annuncio: ' + link_annuncio + '\n\nValutazione Swappie: https://swappie.com/it/vendere/iphone/',
+        category="Integration Test",
+        )
+
+    client = mt.MailtrapClient(token=API_MAIL)
+    client.send(mail)
+  
+
+API_MAIL = os.getenv('API_MAIL')
+MAIL_MIA = os.getenv('MAIL_MIA')
+TOK = os.getenv('TOK')
+LINK_RICERCA = os.getenv('LINK_RICERCA')
+TOK = json.loads(TOK)
+
+SCOPES = ['https://www.googleapis.com/auth/documents']
+creds = service_account.Credentials.from_service_account_info(TOK, scopes=SCOPES)
+DOCUMENT_ID = '1RrJzZSB8OUbmt-vtt-ifk8bKeeC2mUdQQY1d2u6QSp0'
+service = build('docs', 'v1', credentials=creds)
+
+new_content = ''
+
+page = requests.get(LINK_RICERCA)
 soup = BeautifulSoup(page.text, 'html.parser')
 
 product_list_items = soup.find_all('div', class_=re.compile(r'item-card'))
 
 for item in product_list_items:
-    a_tag = item.find('a', class_='SmallCard-module_link__hOkzY')
-    href = a_tag['href'] if a_tag else 'N/A'
+    try:
+        a_tag = item.find('a', class_='SmallCard-module_link__hOkzY')
+        link_annuncio = a_tag['href'] if a_tag else 'N/A'
+        
+        price_tag = item.find('p', class_='index-module_price__N7M2x')
+        prezzo = price_tag.text.strip() if price_tag else 'N/A'
+        prezzo = prezzo.replace('.', '')
+        prezzo = prezzo.replace(',', ' ')
+        prezzo = int(prezzo.split('€')[0])
     
-    price_tag = item.find('p', class_='index-module_price__N7M2x')
-    price = price_tag.text.strip() if price_tag else 'N/A'
-    price = price.replace('.', '')
-    price = price.replace(',', ' ')
-    price = int(price.split('€')[0])
+        title_tag = item.find('h2', class_='index-module_sbt-text-atom__ifYVU')
+        titolo_annuncio = title_tag.text.strip() if title_tag else 'N/A'
     
-    print('URL:', href)
-    print('Prezzo:', price)
-    print('---')
+        if not "cerco" in titolo_annuncio.lower() and not "malfunzionante" in titolo_annuncio.lower() and not "non funzionante" in titolo_annuncio.lower() and not "lcd" in titolo_annuncio.lower() and not "rotta" in titolo_annuncio.lower() and not "schermo" in titolo_annuncio.lower() and not "scocca" in titolo_annuncio.lower() and not "fotocamera" in titolo_annuncio.lower() and not "fotocamere" in titolo_annuncio.lower() and not "display" in titolo_annuncio.lower() and not "scheda madre" in titolo_annuncio.lower() and not "bloccato" in titolo_annuncio.lower() and not "cover per" in titolo_annuncio.lower() and not "guasto" in titolo_annuncio.lower() and not "rotto" in titolo_annuncio.lower() and not "ricambi" in titolo_annuncio.lower() and not "da aggiustare" in titolo_annuncio.lower() and not "no wifi" in titolo_annuncio.lower() and not "display" in titolo_annuncio.lower():
+            if "cover" in titolo_annuncio.lower():
+                if not "con cover" in titolo_annuncio.lower() and not "regalo" in titolo_annuncio.lower() and not "+" in titolo_annuncio.lower() and not "pi" in titolo_annuncio.lower():
+                    continue
+            if "custodia" in titolo_annuncio.lower():
+                if not "con custodia" in titolo_annuncio.lower() and not "regalo" in titolo_annuncio.lower() and not "+" in titolo_annuncio.lower() and not "pi" in titolo_annuncio.lower():
+                    continue
+            if "11" in titolo_annuncio:
+                min = 39
+                max = 121
+            elif "12" in titolo_annuncio:
+                if "mini" in titolo_annuncio.lower():
+                    min = 39
+                    max = 151
+                elif "pro" in titolo_annuncio.lower():
+                    if "max" in titolo_annuncio.lower():
+                        min = 220
+                        max = 340
+                    else:
+                        min = 39
+                        max = 221
+                else:
+                    min = 39
+                    max = 216
+                
+            elif "13" in titolo_annuncio:
+                if "mini" in titolo_annuncio.lower():
+                    min = 170
+                    max = 241
+                elif "pro" in titolo_annuncio.lower():
+                    if "max" in titolo_annuncio.lower():
+                        min = 289
+                        max = 421
+                    else:
+                        min = 199
+                        max = 320
+                else:
+                    min = 199
+                    max = 311
+                    
+            elif "14" in titolo_annuncio:
+                if "pro" in titolo_annuncio.lower():
+                    if "max" in titolo_annuncio.lower():
+                        min = 309
+                        max = 451
+                    else:
+                        min = 289
+                        max = 431
+                else:
+                    min = 209
+                    max = 321
+            else:
+                continue
+                
+            if prezzo > min and prezzo < max:
+                if not link_annuncio in contenuto_file_iniziale + new_content:
+                    #manda_mail(link_annuncio, titolo_annuncio)
+                    new_content = link_annuncio + new_content
+                    aggiorna_file()
+                
+except:
+    pass
+    
